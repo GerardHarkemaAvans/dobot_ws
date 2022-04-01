26209a978a734c4b4347bd39161a3dd50551c83e
Gerard Harkema <ga.harkema@avans.nl>
2022-04-01 14:03:37 +0200

    20220401

---
+++ src/binpicking/binpicking_vision/calculate_objectpose_from_pointcloud.h
@@ -0,0 +1,4 @@
+
+
+
+geometry_msgs::Vector3 calculate_objectpose_from_pointcloud(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud, pcl::PointCloud<pcl::PointXYZRGB>::Ptr &colored_cloud, pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filterd);
